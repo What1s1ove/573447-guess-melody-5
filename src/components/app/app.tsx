939a6ca0fb3403bbb7727ae1969cb5c30c66664e
what1s1ove <zubko.2007@gmail.com>
2020-10-02 07:50:22 +0300

    module2-task1: + app routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,40 @@
 import * as React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
+import AuthScreen from '../auth-screen/auth-screen';
+import GameOverScreen from '../game-over-screen/game-over-screen';
+import WinScreen from '../win-screen/win-screen';
+import { AppRoute } from '~/common/enums/app/app-route.enum';
 
 type Props = {
   errorsCount: number;
 };
 
 const App: React.FC<Props> = ({ errorsCount }) => (
-  <WelcomeScreen errorsCount={errorsCount} />
+  <BrowserRouter>
+    <Switch>
+      <Route exact path={AppRoute.ROOT}>
+        <WelcomeScreen errorsCount={errorsCount} />
+      </Route>
+      <Route exact path={AppRoute.DEV_ARTISTS}>
+        <ArtistQuestionScreen />
+      </Route>
+      <Route exact path={AppRoute.DEV_GENRE}>
+        <GenreQuestionScreen />
+      </Route>
+      <Route exact path={AppRoute.LOGIN}>
+        <AuthScreen />
+      </Route>
+      <Route exact path={AppRoute.RESULT}>
+        <WinScreen />
+      </Route>
+      <Route exact path={AppRoute.LOSE}>
+        <GameOverScreen />
+      </Route>
+    </Switch>
+  </BrowserRouter>
 );
 
 export default App;
