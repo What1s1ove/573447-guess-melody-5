674ed82f37ba248c78b72cce4a3dfb24ec401d8f
what1s1ove <zubko.2007@gmail.com>
2020-10-07 20:16:28 +0300

    module3-task1: + generate-genre-question-answer helper

---
+++ src/helpers/mocks/generate-genre-question-answer.helper.ts
@@ -0,0 +1,11 @@
+import { GenreType } from '~/common/enums/enums';
+import { IGenreQuestionAnswer } from '~/common/interfaces/game/game';
+
+const generateGenreQuestionAnswer = (
+  genre: GenreType
+): IGenreQuestionAnswer => ({
+  genre,
+  src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+});
+
+export { generateGenreQuestionAnswer };
