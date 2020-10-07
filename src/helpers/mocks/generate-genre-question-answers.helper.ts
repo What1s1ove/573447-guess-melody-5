4a369521063a7792bf73ebd55306c399315f9e44
what1s1ove <zubko.2007@gmail.com>
2020-10-07 20:16:44 +0300

    module3-task1: + generate-genre-question-answers helper

---
+++ src/helpers/mocks/generate-genre-question-answers.helper.ts
@@ -0,0 +1,17 @@
+import { generateGenreQuestionAnswer } from '~/helpers/mocks/generate-genre-question-answer.helper';
+import { GenreType } from '~/common/enums/enums';
+import { IGenreQuestionAnswer } from '~/common/interfaces/game/game';
+
+const generateGenreQuestionAnswers = (
+  genres: GenreType[]
+): IGenreQuestionAnswer[] => {
+  const genreQuestionAnswers = genres.map((genre) => {
+    const genreQuestionAnswer = generateGenreQuestionAnswer(genre);
+
+    return genreQuestionAnswer;
+  });
+
+  return genreQuestionAnswers;
+};
+
+export { generateGenreQuestionAnswers };
