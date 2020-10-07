f5ce9becc840f1942f0c1ede73076817a2ceccd1
what1s1ove <zubko.2007@gmail.com>
2020-10-07 20:30:04 +0300

    module3-task1: + artist name enum

---
+++ src/common/enums/game/artist-name.enum.ts
@@ -0,0 +1,9 @@
+enum ArtistName {
+  JIM_BEAM = `Jim Beam`,
+  JOHN_SNOW = `John Snow`,
+  JACK_DANIELS = `Jack Daniels`,
+  SERCEI_LANNISTER = `Cersei Lannister`,
+  BILLIE_EILISH = `Billie Eilish`,
+}
+
+export { ArtistName };
