feed1ec25c932538ecf7c2388cf123722fb3ee3d
what1s1ove <zubko.2007@gmail.com>
2020-10-08 20:58:53 +0300

    module3-task: + game-screen component

---
+++ src/components/game-screen/game-screen.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { Redirect } from 'react-router-dom';
+import { AppRoute, QuestionType } from '~/common/enums/enums';
+import { GameQuestion } from '~/common/types/types';
+import GenreQuestionScreen from '~/components/genre-question-screen/genre-question-screen';
+import ArtistQuestionScreen from '~/components/artist-question-screen/artist-question-screen';
+
+const DEFAULT_STEP = 0;
+const INCREMENT_STEP_COUNT = 1;
+
+type Props = {
+  questions: GameQuestion[];
+};
+
+const GameScreen: React.FC<Props> = ({ questions }) => {
+  const [step, setStep] = React.useState<number>(DEFAULT_STEP);
+  const currentQuestion = React.useMemo(() => questions[step], [
+    step,
+    questions,
+  ]);
+
+  const onAnswer = React.useCallback(() => {
+    const newStep = step + INCREMENT_STEP_COUNT;
+
+    setStep(newStep);
+  }, [step]);
+
+  const getScreen = (question: GameQuestion) => {
+    switch (question.type) {
+      case QuestionType.GENRE: {
+        return <GenreQuestionScreen question={question} onAnswer={onAnswer} />;
+      }
+      case QuestionType.ARTIST: {
+        return <ArtistQuestionScreen question={question} onAnswer={onAnswer} />;
+      }
+    }
+  };
+
+  if (step >= questions.length || !currentQuestion) {
+    return <Redirect to={AppRoute.ROOT} />;
+  }
+
+  return getScreen(currentQuestion);
+};
+
+export default GameScreen;
