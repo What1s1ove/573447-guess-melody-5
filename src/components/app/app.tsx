46430c71b3f5af6174e72bd6bac5e0a1e93269aa
what1s1ove <zubko.2007@gmail.com>
2020-10-08 21:35:05 +0300

    module3-task: + game-screen to site route

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,6 +3,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { AppRoute } from '~/common/enums/enums';
 import { GameQuestion } from '~/common/types/types';
 import WelcomeScreen from '~/components/welcome-screen/welcome-screen';
+import GameScree from '~/components/game-screen/game-screen';
 import AuthScreen from '~/components/auth-screen/auth-screen';
 import GameOverScreen from '~/components/game-over-screen/game-over-screen';
 import WinScreen from '~/components/win-screen/win-screen';
@@ -12,7 +13,7 @@ type Props = {
   questions: GameQuestion[];
 };
 
-const App: React.FC<Props> = ({ errorsCount }) => (
+const App: React.FC<Props> = ({ questions, errorsCount }) => (
   <BrowserRouter>
     <Switch>
       <Route exact path={AppRoute.ROOT}>
@@ -20,6 +21,9 @@ const App: React.FC<Props> = ({ errorsCount }) => (
       </Route>
       <Route exact path={AppRoute.LOGIN}>
         <AuthScreen />
+      </Route>
+      <Route exact path={AppRoute.GAME}>
+        <GameScree questions={questions} />
       </Route>
       <Route exact path={AppRoute.RESULT}>
         <WinScreen />
