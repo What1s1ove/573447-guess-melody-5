5a0c103cb509938263bc20474badadceb85288fb
what1s1ove <zubko.2007@gmail.com>
2020-10-24 10:22:04 +0300

    module6-task1: * game-over-screen component

--- src/components/game-over-screen/game-over-screen.tsx
+++ src/components/game-over-screen/game-over-screen.tsx
@@ -1,23 +1,37 @@
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { AppRoute } from '~/common/enums/enums';
+import { GameActionCreator } from '~/store/actions/actions';
 
-const GameOverScreen = () => (
-  <section className="result">
-    <div className="result__logo">
-      <img
-        src="img/melody-logo.png"
-        alt="Угадай мелодию"
-        width="186"
-        height="83"
-      />
-    </div>
-    <h2 className="result__title">Какая жалость!</h2>
-    <p className="result__total result__total--fail">
-      У вас закончились все попытки. Ничего, повезёт в следующий раз!
-    </p>
-    <button className="replay" type="button">
-      Попробовать ещё раз
-    </button>
-  </section>
-);
+const GameOverScreen = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const handleGameReset = () => {
+    dispatch(GameActionCreator.resetGame());
+    history.push(AppRoute.GAME);
+  };
+
+  return (
+    <section className="result">
+      <div className="result__logo">
+        <img
+          src="img/melody-logo.png"
+          alt="Угадай мелодию"
+          width="186"
+          height="83"
+        />
+      </div>
+      <h2 className="result__title">Какая жалость!</h2>
+      <p className="result__total result__total--fail">
+        У вас закончились все попытки. Ничего, повезёт в следующий раз!
+      </p>
+      <button className="replay" onClick={handleGameReset} type="button">
+        Попробовать ещё раз
+      </button>
+    </section>
+  );
+};
 
 export default GameOverScreen;
