886fae67b0a82ca4d352056dce245d59df319551
what1s1ove <zubko.2007@gmail.com>
2020-10-30 14:57:11 +0200

    module7-task1: + thunk middleware config

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { AuthStatus } from '~/common/enums/enums';
+import { createAPI } from '~/services/api/api';
+import { UserActionCreator } from '~/store/actions/actions';
 import rootReducer from './reducer.root';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const api = createAPI({
+  onUnauthorized: () => {
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    store.dispatch(UserActionCreator.setAuthStatus(AuthStatus.NO_AUTH));
+  },
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk.withExtraArgument(api)))
+);
 
 export default store;
