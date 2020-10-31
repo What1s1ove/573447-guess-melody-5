31aef755f4fb590530a5118fef646030811f0752
what1s1ove <zubko.2007@gmail.com>
2020-10-31 12:17:37 +0200

    module7-task1: * redirect middleware

--- src/store/middlewares/redirect.ts
+++ src/store/middlewares/redirect.ts
@@ -2,10 +2,13 @@ import { Middleware } from 'redux';
 import { RootState } from '~/store/reducer.root';
 import browserHistory from '~/browser-history';
 import { UserActionType } from '~/common/enums/enums';
+import { RedirectToRouteAction } from '~/common/types/types';
 
 const redirect: Middleware<{}, RootState> = () => (next) => (action) => {
   if (action.type === UserActionType.REDIRECT_TO_ROUTE) {
-    browserHistory.push(action.payload);
+    const currentAction = action as RedirectToRouteAction;
+
+    browserHistory.push(currentAction.payload.path);
   }
 
   return next(action);
