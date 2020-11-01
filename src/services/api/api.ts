3683f7aabd1954296ba4027bf679b105592eb098
what1s1ove <zubko.2007@gmail.com>
2020-11-01 13:05:28 +0200

    module7-task1: * base url var name

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -2,11 +2,11 @@ import axios, { AxiosResponse, AxiosError } from 'axios';
 import { HttpCode } from '~/common/enums/enums';
 import { BindingCb } from '~/common/types/types';
 
-const BACKEND_URL = `https://5.react.pages.academy/guess-melody`;
+const BASE_URL = `https://5.react.pages.academy/guess-melody`;
 const REQUEST_TIMEOUT = 5000;
 
 const apiInstance = axios.create({
-  baseURL: BACKEND_URL,
+  baseURL: BASE_URL,
   timeout: REQUEST_TIMEOUT,
   withCredentials: true,
 });
