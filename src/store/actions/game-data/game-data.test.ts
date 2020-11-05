96732c06f133bc6716368e793f738c3299e75139
what1s1ove <zubko.2007@gmail.com>
2020-11-05 19:28:07 +0200

    module8-task1: + tests for async game-data ac

--- src/store/actions/game-data/game-data.test.ts
+++ src/store/actions/game-data/game-data.test.ts
@@ -1,6 +1,12 @@
-import { GameDataActionType } from '~/common/enums/enums';
+import MockAdapter from 'axios-mock-adapter';
+import { createAPI } from '~/services/api/api';
+import { ApiRoute, GameDataActionType, HttpCode } from '~/common/enums/enums';
 import { mockedQuestions } from '~/mocks/mocks';
 import { GameDataActionCreator } from './game-data';
+
+const api = createAPI({
+  onUnauthorized: jest.fn(),
+});
 
 describe(`Game-data action creator works correctly`, () => {
   it(`loadQuestions ac returns correct action`, () => {
@@ -9,6 +15,25 @@ describe(`Game-data action creator works correctly`, () => {
       payload: {
         questions: mockedQuestions,
       },
+    });
+  });
+
+  it(`login ac returns correct action. Should make a correct API call to /questions`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const questionLoader = GameDataActionCreator.fetchQuestions();
+
+    apiMock.onGet(ApiRoute.QUESTIONS).reply(HttpCode.SUCCESS, mockedQuestions);
+
+    return questionLoader(dispatch, jest.fn(), { api }).then(() => {
+      expect(dispatch).toHaveBeenCalledTimes(1);
+
+      expect(dispatch).toHaveBeenNthCalledWith(1, {
+        type: GameDataActionType.LOAD_QUESTIONS,
+        payload: {
+          questions: mockedQuestions,
+        },
+      });
     });
   });
 });
